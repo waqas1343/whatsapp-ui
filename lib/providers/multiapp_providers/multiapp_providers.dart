dcf9af7a3c42e79a061a3c397defdc4276f3565a
waqas1343 <waqasilyas5904@gmail.com>
2025-03-13 02:28:25 +0500

    first

--- lib/providers/multiapp_providers/multiapp_providers.dart
+++ lib/providers/multiapp_providers/multiapp_providers.dart
@@ -1,9 +1,7 @@
 import 'package:medichat/providers/controllers/otpHover/opt_hover.dart';
-import 'package:medichat/providers/controllers/terms_agrement/termsagrement.dart';
 import 'package:provider/provider.dart';
 
 final List<ChangeNotifierProvider> multiAppProvider = [
-  ChangeNotifierProvider<Termsagrement>(create: (context) => Termsagrement()),
   ChangeNotifierProvider<OptHoverProvider>(
     create: (context) => OptHoverProvider(),
   ),
