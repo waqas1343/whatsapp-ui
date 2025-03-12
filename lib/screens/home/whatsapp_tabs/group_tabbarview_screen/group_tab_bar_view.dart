5a8d8e025564e37a79cb20d06dbc4ee95cff62c6
waqas1343 <waqasilyas5904@gmail.com>
2025-03-13 03:31:30 +0500

    chat screen done

---
+++ lib/screens/home/whatsapp_tabs/group_tabbarview_screen/group_tab_bar_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class GroupTabBarView extends StatelessWidget {
+  const GroupTabBarView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.tealAccent,
+
+      body: Center(child: Text('group')),
+    );
+  }
+}
