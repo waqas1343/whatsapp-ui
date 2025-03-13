6d1ca7143f07519a463625cbc3ad64971784fb71
waqas1343 <waqasilyas5904@gmail.com>
2025-03-14 01:28:50 +0500

    gimini chat

--- lib/providers/multiapp_providers/multiapp_providers.dart
+++ lib/providers/multiapp_providers/multiapp_providers.dart
@@ -1,3 +1,4 @@
+import 'package:medichat/providers/controllers/ai_chat_provider/ai_chat_provider.dart';
 import 'package:medichat/providers/controllers/otpHover/opt_hover.dart';
 import 'package:provider/provider.dart';
 
@@ -5,4 +6,5 @@ final List<ChangeNotifierProvider> multiAppProvider = [
   ChangeNotifierProvider<OptHoverProvider>(
     create: (context) => OptHoverProvider(),
   ),
+  ChangeNotifierProvider<ChatProvider>(create: (context) => ChatProvider()),
 ];
