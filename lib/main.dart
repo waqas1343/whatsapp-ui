8f74ac2b817691dc70af9d242da269e8055180c3
Shahzaibkhan80 <kamshahzaib@gmail.com>
2025-03-26 23:51:34 +0500

    asy

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class MyApp extends StatelessWidget {
             theme: AppTheme.lightTheme,
             darkTheme: AppTheme.darkTheme,
             themeMode: ThemeMode.system,
-            home: AiChatScreen(),
+            home: WelcomeScreen(),
           ),
         );
       },
